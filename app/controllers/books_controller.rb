4c390068a604e9d0ea513398bb1cba07ed447116
Syun Tanaka <raityupiyo@MacBook-Pro-3.local>
2020-01-16 13:52:33 +0900

    [FIX]エラーメッセージの修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,13 +9,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
       flash[:success] = "Book was successfully created."
       redirect_to book_path(book)
     else
-      flash[:success] = "can't be blank"
-      redirect_to books_path
+      render action: :index
     end
   end
 
@@ -28,13 +28,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    @books = Book.all
+    if @book.update(book_params)
       flash[:success] = "Book was successfully updated."
       redirect_to book_path(book.id)
     else 
-      flash[:success] = "Failed to updated Book."
-      redirect_to books_path
+      render action: :edit
     end
   end
 
